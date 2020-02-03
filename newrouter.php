ceaaf7c7083b514d82120f49db1ed973f612fc8c
ferrriii <57658129+ferrriii@users.noreply.github.com>
2020-02-03 10:39:29 +0330

    Remove queries from URL before matching route

--- newrouter.php
+++ newrouter.php
@@ -131,6 +131,7 @@ class NewRouter {
 		if ($path === null) {
 			$path = $_SERVER['REQUEST_URI'];
 		}
+		$path = strtok($_SERVER["REQUEST_URI"],'?');
 		
 		return $this->execute($method, $path, $prefixPattern);
 	}
